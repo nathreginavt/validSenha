440e91db9228153d45a43f278d0c35db74e839ea
Nathália Teixeira <nathalia.reginavt@gmail.com>
2024-12-17 14:57:01 -0300

    Feat: Adc teste integracao

--- src/test/java/com/example/demo/ValidSenhaApplicationTests.java
+++ src/test/java/com/example/demo/ValidSenhaApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.controller.ValidatePasswordController;
 
 @SpringBootTest
 class ValidSenhaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+    private ValidatePasswordController controller;
+
+    @Test
+    public void testPasswordValidationIntegration() {
+        boolean result = controller.validatePassword("AbTp9!fok");
+        assertTrue(result);
+    }
 
 }
