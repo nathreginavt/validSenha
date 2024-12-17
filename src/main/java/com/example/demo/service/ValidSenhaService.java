464ac2a327b0371e788ae1c4a14d0e16267276fd
Nathália Teixeira <nathalia.reginavt@gmail.com>
2024-12-17 00:01:21 -0300

    Fix: Otimizar service

--- src/main/java/com/example/demo/service/ValidSenhaService.java
+++ src/main/java/com/example/demo/service/ValidSenhaService.java
@@ -2,21 +2,22 @@ package com.example.demo.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ValidSenhaService{
+public class ValidSenhaService {
 
     public boolean isValid(String senha){
 
-        boolean temQtdCaracteres = senha.length() >= 9;
+        if(senha == null || senha.length() < 9 || senha.contains(" ") ){
+            return false;
+        }
+
         boolean temDigito = senha.matches(".*[0-9].*");
         boolean temMaiuscula = senha.matches(".*[A-Z].*");
         boolean temMinuscula = senha.matches(".*[a-z].*");
         boolean temCaracterEspecial = senha.matches(".*[!@#$%^&*()-+].*");
-        boolean naoRepete = !senha.matches(".*(.)\\1.*");  //nao funciona
-        boolean naoTemEspacos = !senha.contains(" ");
+        boolean naoRepete = !senha.matches(".*(.)\\1.*");
 
-        return temQtdCaracteres && temDigito &&
-                 temMaiuscula && temMinuscula && temCaracterEspecial &&
-                  naoRepete && naoTemEspacos; 
+        return temDigito && temMaiuscula && temMinuscula 
+            && temCaracterEspecial && naoRepete; 
 
     }
 }
